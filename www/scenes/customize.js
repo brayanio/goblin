0d4dc00e4b86160d87431e1a53a31f4095c3adff
Brayan Byrdsong <brayanbyrdsong@gmail.com>
2020-04-08 04:28:51 +0000

    style changes

--- www/scenes/customize.js
+++ www/scenes/customize.js
@@ -3,25 +3,23 @@ import Data from '../../data/module.js'
 import Prefabs from '../prefabs/customize.js'
 import CustomHeroPipe from '../pipes/custom-hero.js'
 
-export default () => {
-  const tab = nggt.dataObj('step1')
-  return nggt.create({
-    isRoot: true,
-    classList: ['customize', 'gl-scene_mobile_pro'],
-    template: Prefabs.Card(
+export default () => nggt.create({
+  isRoot: true,
+  classList: ['customize', 'gl-scene_mobile_pro'],
+  template: Prefabs.Card(
+    Prefabs.Container('div', ['gl-card', 'character-sheet'],
       Prefabs.ColGrid(0, 10, 10,
         Prefabs.HeroForm('Character Sheet') + Prefabs.HeroStats(),
         Prefabs.HeroRace() + Prefabs.HeroEquipment(),
         Prefabs.DataObj(CustomHeroPipe.selectedItem, i => 
           i && Prefabs.ItemInspect(i)
         )
-      ),
-      Prefabs.HeroStore(),
-      Prefabs.Container('div', ['right'],
-        Prefabs.Btn(['gl-btn_icon'], 'Back', tab.onevent('step1')),
-        Prefabs.Btn(['gl-btn_icon'], 'Create', () => console.log(CustomHeroPipe.unit(), new Data.GameUnit(CustomHeroPipe.unit())))
       )
     ),
-    cleanup: () => CustomHeroPipe.cleanup()
-  })
-}
+    Prefabs.HeroStore(),
+    Prefabs.Container('div', ['right'],
+      Prefabs.Btn(['gl-btn_icon'], 'Create', () => console.log(new Data.GameUnit(CustomHeroPipe.unit())))
+    )
+  ),
+  cleanup: () => CustomHeroPipe.cleanup()
+})
